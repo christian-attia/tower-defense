a83518865a807da369740943afcce7290a802a3f
CrisDossierPJ <christian_attia@hotmail.com>
2017-03-28 15:00:29 +0200

    Update perlin.js

--- Map/perlin.js
+++ Map/perlin.js
@@ -1,110 +1,134 @@
 var inc = 0.0045;
 
 function setup() {
-
-  createCanvas(1300 , 900);
+  background(0);
   pixelDensity(1);
-
-
 }
 
 
 function draw()
 {
-
-  var ctx = canvas.getContext("2d");
-
-  var yoff = 0;
-  var tailleCases = 25;
-
-  var tabEntreX = 0;
-  var tabEntreY = 0;
-
-  var tabSortieX = 51;
-  var tabSortieY = 35;
-
-  var entreeX = 0;
-  var entreeY = 0;
-  var sortieX = 1300 - tailleCases;
-  var sortieY = 900 - tailleCases;
-
-  var plaine = 0;
-  var montagne = 0;
-  var ocean = 0;
-  var plage = 0;
-  var tabR = new Array();
-  var tabCoord =new Array();
-
-  for (var i=0; i<1871; i++) {
+  
+  
+  vie = 100;
+  
+  
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  
+  
+  
+  ennemiMortImg = new Image();
+  montagneImg = new Image();
+  plaineImg = new Image();
+  oceanImg = new Image();
+  cheminImg = new Image();
+  ennemiImg = new Image();
+  mapImg = new Image();
+  montagneImg.src = 'images/montagne.jpg';
+  plaineImg.src = 'images/plaine.jpg';
+  oceanImg.src = 'images/ocean.jpg';
+  cheminImg.src = 'images/chemin.jpg';
+  ennemiImg.src = 'images/ennemi.jpg';
+  mapImg.src = 'images/map.png';
+  ennemiMortImg.src = 'images/ennemiMort.jpg';
+  
+  
+  yoff = 0;
+  tailleCases = 25;
+  
+  tabEntreX = 0;
+  tabEntreY = 0;
+  
+  tabSortieX = 51;
+  tabSortieY = 35;
+  
+  entreeX = 0;
+  entreeY = 0;
+  sortieX = 1300 - tailleCases;
+  sortieY = 900 - tailleCases;
+  
+  plaine = 0;
+  montagne = 0;
+  ocean = 0;
+  plage = 0;
+  tabR = new Array();
+  tabCoord =new Array();
+  tabColori = new Array();
+  
+  for (var i=0; i<1871; i++)
+  {
     tabCoord[i] = new Array();
-    for (var j=0; j<1871; j++) {
+    for (var j=0; j<1871; j++)
+    {
       tabCoord[i][j] = "Vide";
     }
   }
-
-  var coordX = -1;
-  var coordY = -1;
-
-
+  for (var i=0; i<51; i++)
+  {
+    tabColori[i] = new Array(35);
+  }
+  
+  
+  
+  
+  coordX = -1;
+  coordY = -1;
+  
+  
   loadPixels();
-
+  
   for (var y = 0; y < 900; y++)
   {
-        var xoff = 0;
-
+    var xoff = 0;
+    
     for (var x = 0; x < 1300; x++)
     {
-
+      
       var index = (x + y * 1300) * 4;
       //var r = random(255);
       var r = noise(xoff, yoff) * 255;
-
+      
       tabR[x+(y*1300)] = r;
       //console.log(tabR[x]);
-
+      
       pixels[index ] = r
       pixels[index + 1 ] = r
       pixels[index + 2 ] = r
       pixels[index + 3] = 255;
-
-
+      
+      
       if ( r < 255 &&  r >= 170) //ocean.
       {
-       pixels[index + 0] = 0;
-       pixels[index + 1] = 0;
-       pixels[index + 2] = 204;
-       ocean++;
+        
+        ocean++;
       }
-       if ( r < 170 &&  r > 85) //plaine.
+      if ( r < 170 &&  r > 85) //plaine.
       {
-       pixels[index + 0] = 0;
-       pixels[index + 1] = 100;
-       pixels[index + 2] = 0;
-       plaine++;
-       }
-        if ( r < 85 &&  r > 0) //montagne.
-       {
-       pixels[index + 0] = 0;
-       pixels[index + 1] = 0;
-       pixels[index + 2] = 0;
-       montagne++;
-       }
-
-
-
-
+        
+        montagne++;
+      }
+      if ( r < 85 &&  r > 0) //montagne.
+      {
+        
+        plaine++;
+      }
+      
       xoff += inc;
     }
-     yoff += inc;
+    yoff += inc;
   }
-  updatePixels();
-
-console.log(montagne);
+  //updatePixels();
+  
+  //console.log(montagne);
   if(montagne < 230000)
   {
-    window.location.reload()
+    //alert('Pas assez de montagnes');
+    ///  window.location.reload()
   }
-
+  
+  //coloriCanvas(-1,-1);
+  
   for(var a = 0; a<900; a+=tailleCases)// faire des bons en hauteur
   {
     coordY++;
@@ -115,269 +139,412 @@ console.log(montagne);
       var vert=0;
       var bleu = 0;
       var noir =0;
-
+      
       for(var g = 0; g < tailleCases; g++)//on s'occupe pixel par pixel hauteur
       {
-         ocean = 0;
-         plaine = 0;
-         montagne = 0;
-
-          for(var h = 0; h < tailleCases; h++)//on s'occupe pixel par pixel largeur
+        ocean = 0;
+        plaine = 0;
+        montagne = 0;
+        for(var h = 0; h < tailleCases; h++)//on s'occupe pixel par pixel largeur
+        {
+          var pix = h+(g*1300)+b+(a*1300);// h = Pour aller de 0 a 50, ensuite on va au pixels numéro 1300
+          var y = tabR[pix];
+          if ( y< 255 &&  y >= 170) //ocean.
           {
-            var pix = h+(g*1300)+b+(a*1300);// h = Pour aller de 0 a 50, ensuite on va au pixels numéro 1300
-            var y = tabR[pix];
-
-            if ( y< 255 &&  y >= 170) //ocean.
-            {
-              ocean++;
-            }
-
-            if ( y < 170 &&  y > 85) //plaine.
-            {
-             plaine++;
-            }
-
-            if ( y <= 85 &&  y >= 0) //montagne
-            {
-             montagne++;
-            }
-
+            ocean++;
           }
-
+          if ( y < 170 &&  y > 85) //plaine.
+          {
+            plaine++;
+          }
+          if ( y <= 85 &&  y >= 0) //montagne
+          {
+            montagne++;
+          }
+        }
         if(montagne > plaine && montagne > ocean )
         {
-           noir++;
+          noir++;
         }
-
         if(plaine > montagne && plaine > ocean)
         {
           vert++;
         }
-
         if(ocean > plaine && ocean > montagne)
         {
-         bleu++;
+          bleu++;
         }
-
       }
-
-       /*console.log("montagne " + noir);
-             console.log("plaine " + vert );
-             console.log("ocean " + bleu);*/
-     if(noir>vert && noir>bleu)
-     {
-     ctx.beginPath();
-     ctx.fillStyle = "#996633";
-     ctx.fillRect(b,a,tailleCases,tailleCases);
-     ctx.closePath();
-
-     
-
-     tabCoord[coordX][coordY] = "montagne";
-
-
-     if(coordX >= 2)
-     {
-       if(tabCoord[coordX - 2][coordY] == "montagne" && tabCoord[coordX][coordY] == "montagne")
-       {
-         ctx.beginPath();
-         ctx.fillStyle = "#996633";
-         ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-         ctx.closePath();
-         tabCoord[coordX-1][coordY]=="montagne";
-       }
-
-
-     }
-     if(coordY >= 2)
-     {
-     if(tabCoord[coordX ][coordY - 2] == "montagne" && tabCoord[coordX ][coordY] == "montagne")
-     {
-       ctx.beginPath();
-       ctx.fillStyle = "#996633";
-       ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-       ctx.closePath();
-       tabCoord[coordX][coordY-1]=="montagne";
-     }
-    }
-
-
-     }
-    else if(vert>noir && vert>bleu)
-     {
-     ctx.beginPath();
-     ctx.fillStyle = "green";
-     ctx.fillRect(b,a,tailleCases,tailleCases);
-     ctx.closePath();
-     tabCoord[coordX][coordY] = "plaine";
-
-         if(coordX >= 2)
-         {
-         if(tabCoord[coordX - 2][coordY] == "plaine" && tabCoord[coordX ][coordY] == "plaine")
-         {
-           ctx.beginPath();
-           ctx.fillStyle = "green";
-           ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-           ctx.closePath();
-           tabCoord[coordX-1][coordY]=="plaine";
-         }
-        }
-        if(coordY >= 2)
+      if(noir>vert && noir>bleu)
+      {
+        tabCoord[coordX][coordY] = "montagne";
+        /*
+        if(coordX >= 2)
         {
-        if(tabCoord[coordX ][coordY - 2] == "plaine" && tabCoord[coordX ][coordY] == "plaine")
+        if(tabCoord[coordX - 2][coordY] == "montagne" && tabCoord[coordX][coordY] == "montagne")
         {
-          ctx.beginPath();
-          ctx.fillStyle = "green";
-          ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-          ctx.closePath();
-          tabCoord[coordX][coordY-1]=="plaine";
-        }
-       }
-
-     }
-     else if(bleu>vert && bleu>noir)
-     {
-     ctx.beginPath();
-     ctx.fillStyle = "blue";
-     ctx.fillRect(b,a,tailleCases,tailleCases);
-     ctx.closePath();
-     tabCoord[coordX][coordY] = "ocean";
-
-     if(coordX >= 2)
-     {
-     if(tabCoord[coordX - 2][coordY] == "ocean" && tabCoord[coordX ][coordY] == "ocean")
-     {
-       ctx.beginPath();
-       ctx.fillStyle = "blue";
-       ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-       ctx.closePath();
-       tabCoord[coordX-1][coordY]="ocean";
-     }
-   }
-   if(coordY >= 2)
-   {
-   if(tabCoord[coordX ][coordY - 2] == "ocean" && tabCoord[coordX ][coordY] == "ocean")
-   {
-     ctx.beginPath();
-     ctx.fillStyle = "blue";
-     ctx.fillRect(b-tailleCases,a,tailleCases,tailleCases);
-     ctx.closePath();
-     tabCoord[coordX][coordY-1]=="ocean";
-   }
-  }
-
-     }
-     else
-     {
-     ctx.beginPath();
-     ctx.fillStyle = "#996633";
-     ctx.fillRect(b,a,tailleCases,tailleCases);
-     ctx.closePath();
-     /*tabCoord[numTest]="montagne";*/
-     }
-     //console.log(tabCoord[coordX][coordY] + " X : " + coordX + " Y : " + coordY);
-
-    }
-  }
-
-
-  //création entrée sortie ennemis.
-  coordX = 0;
-  coordY = 0;
-  while(tabCoord[tabEntreX][tabEntreY] == "montagne" || tabCoord[tabEntreX][tabEntreY] == "ocean")
-  {
-    coordX++;
-    tabEntreX++;
-  }
-  var ReCoordXPF = coordX;
-  var ReCoordYPF = coordY;
-  tabCoord[tabEntreX][tabEntreY] = "Entrée";
-//  console.log(tabCoord[tabEntreX][tabEntreY] + " X : " + coordX + " Y " +coordY);
-  ctx.beginPath();
-  ctx.fillStyle = "yellow";
-  ctx.fillRect(tabEntreX*tailleCases,entreeY,tailleCases,tailleCases);
-  ctx.closePath();
-
-
-  coordX = 51;
-  coordY = 35;
-  while(tabCoord[tabSortieX][tabSortieY] == "montagne" || tabCoord[tabSortieX][tabSortieY]== "ocean")
-  {
-    coordX--;
-    sortieX -= tailleCases;
-    tabSortieX--;
-  }
-  var coordXPF = coordX - 1;
-  var coordYPF = coordY - 1;
-  tabCoord[tabEntreX][tabEntreY] = "Sortie";
-  //console.log(tabCoord[tabEntreX][tabEntreY] + " X : " + coordX + " Y " +coordY);
-  ctx.beginPath();
-  ctx.fillStyle = "yellow";
-  ctx.fillRect(sortieX,sortieY,tailleCases,tailleCases);
-  ctx.closePath();
-
- var grille = new PF.Grid(51,35);
-
- for (var i=0; i<51; i++) {
-   grille[i] = new Array(35);
- }
-
-
- for(var i = 0; i<35;i++){
-   for(var j = 0; j<51;j++){
-          grille[j][i] = tabCoord[j][i];
-          }
-      }
-
-    for(var i =0; i<35; i++){
-      for(var j = 0; j<51; j++){
-            if(tabCoord[j][i] === "montagne" || tabCoord[j][i] ==="ocean"){
-            grille.setWalkableAt(j , i , false);
-            }
-            else if(tabCoord[j][i] === "plaine" ){
-            grille.setWalkableAt(j , i , true);
-            }
+        tabCoord[coordX-1][coordY]=="montagne";
       }
     }
-
-var finder = new PF.AStarFinder();
-var positionX = 0;
-var positionY = 0;
-var path = finder.findPath(ReCoordXPF,ReCoordYPF,coordXPF,coordYPF,grille);
-
-/*
-for(var i = 0; i<path.length;i++){
-  for(var j = 0; j<2;j++){
-console.log( path[i][j]);
+    */
+  }
+  if(vert>noir && vert>bleu)
+  {
+    tabCoord[coordX][coordY] = "plaine";
+    /*
+    if(coordX >= 2)
+    {
+    if(tabCoord[coordX - 2][coordY] == "plaine" && tabCoord[coordX ][coordY] == "plaine")
+    {
+    tabCoord[coordX-1][coordY]=="plaine";
+  }
+}
+*/
+}
+if(bleu>vert && bleu>noir)
+{
+  tabCoord[coordX][coordY] = "ocean";
+  /*
+  if(coordX >= 2)
+  {
+  if(tabCoord[coordX - 2][coordY] == "ocean" && tabCoord[coordX ][coordY] == "ocean")
+  {
+  tabCoord[coordX-1][coordY]="ocean";
 }
 }
 */
-//console.log(path.length);
-  if (path.length == 0)
-  {
-  window.location.reload()
-
-  }
-
-for(var i = 0; i<path.length;i++){
-  for(var j = 0; j<2;j++){
-    if (j==0){
-            positionX = path[i][j];
-              }
-    else{
-       positionY=path[i][j];
-        }
-  }
-    //console.log("X " + positionX + " Y "+ positionY);
-  ctx.beginPath();
-  ctx.fillStyle = "yellow";
-  ctx.fillRect(positionX * 25,positionY * 25,tailleCases,tailleCases);
-  ctx.closePath();
 }
-ctx.beginPath();
-ctx.fillStyle = "yellow";
-ctx.fillRect((positionX + 1) * 25,positionY * 25,tailleCases,tailleCases);
-ctx.closePath();
+}
+}
 
-  noLoop();
+//création entrée sortie ennemis.
+
+coordX = 0;
+coordY = 0;
+
+while(tabCoord[tabEntreX][tabEntreY] == "montagne" || tabCoord[tabEntreX][tabEntreY] == "ocean")
+{
+  coordX++;
+  entreeX += tailleCases;
+  tabEntreX++;
+}
+ReCoordXPF = coordX;
+ReCoordYPF = coordY;
+tabCoord[tabEntreX][tabEntreY] = "Entrée";
+//  console.log(tabCoord[tabEntreX][tabEntreY] + " X : " + coordX + " Y " +coordY);
+
+coordX = 51;
+coordY = 35;
+
+while(tabCoord[tabSortieX][tabSortieY] == "montagne" || tabCoord[tabSortieX][tabSortieY]== "ocean")
+{
+  coordX--;
+  sortieX -= tailleCases;
+  tabSortieX--;
+}
+coordXPF = coordX -1 ;
+coordYPF = coordY - 1;
+tabCoord[tabEntreX][tabEntreY] = "Sortie";
+
+
+
+var grille = new PF.Grid(51,35);
+
+for (var i=0; i<51; i++)
+{
+  grille[i] = new Array(35);
+}
+
+
+for(var i = 0; i<35;i++)
+{
+  for(var j = 0; j<51;j++)
+  {
+    grille[j][i] = tabCoord[j][i];
+  }
+}
+
+for(var i =0; i<35; i++)
+{
+  for(var j = 0; j<51; j++)
+  {
+    if(tabCoord[j][i] === "montagne" || tabCoord[j][i] ==="ocean")
+    {
+      grille.setWalkableAt(j , i , false);
+    }
+    else if(tabCoord[j][i] === "plaine" )
+    {
+      grille.setWalkableAt(j , i , true);
+    }
+  }
+}
+
+var finder = new PF.AStarFinder({allowDiagonal : true, dontCrossCorners : true});
+var positionX = 0;
+var positionY = 0;
+path = finder.findPath(ReCoordXPF,ReCoordYPF,coordXPF,coordYPF,grille);
+
+
+if (path.length == 0)
+{
+  window.location.reload()
+}
+cheminImg.onload = function () {
+  for(var i = 0; i<path.length;i++)
+  {
+    for(var j = 0; j<2;j++)
+    {
+      if (j==0)
+      {
+        positionX = path[i][j];
+      }
+      else
+      {
+        positionY=path[i][j];
+      }
+    }
+    //ctx.drawImage(cheminImg,positionX * 25,positionY * 25,tailleCases,tailleCases);
+    //grille[i][j] = "chemin";
+  }
+  //ctx.drawImage(cheminImg,(positionX )* 25,(positionY+1) * 25,tailleCases,tailleCases);
+  //  grille[i][j] = "chemin";
+}
+
+console.log(grille);
+noLoop();
+}
+
+window.requestAnimationFrame(ennemi);
+
+var i = 0;
+var j = 0;
+
+function ennemi(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  coloriCanvas(-1,-1);
+  
+  ctx.drawImage(cheminImg,sortieX , sortieY,tailleCases,tailleCases);
+  ctx.drawImage(cheminImg,sortieX - 25 ,sortieY,tailleCases,tailleCases);
+  var v = path.length - 1;
+  console.log(v);
+  if(i == v )
+  {
+    ctx.drawImage(ennemiImg,sortieX-25,sortieY,tailleCases,tailleCases);
+    sleep(100);
+  }
+  
+  if(i == path.length){
+    ctx.drawImage(ennemiMortImg,sortieX, sortieY,tailleCases,tailleCases);
+    path.length ++;
+    i++;
+  }
+  if(i == path.lenght + 1)
+  {
+    ///ctx.drawImage(cheminImg,sortieX , sortieY,tailleCases,tailleCases);
+  }
+  
+  
+  if(i<path.length){
+    j = 0;
+    while(j<2){
+      if (j==0){
+        positionX = path[i][j];
+      }
+      else{
+        positionY=path[i][j];
+      }
+      j++;
+    }
+    if(keyIsPressed === true)
+    {
+      
+      ctx.drawImage(ennemiImg,(positionX - 3 ) * 25,(positionY) * 25,tailleCases,tailleCases);
+      
+    }
+    else{
+      ctx.drawImage(ennemiImg,positionX * 25,positionY * 25,tailleCases,tailleCases);
+    }
+    sleep(100);
+    
+    i++;
+    
+    window.requestAnimationFrame(ennemi);
+    
+  }
+  
+}
+
+
+function coloriCanvas(coordX,coordY){
+  
+  for(var a = 0; a<900; a+=tailleCases)// faire des bons en hauteur
+  {
+    coordY++;
+    coordX = -1;
+    for(var i = 0; i<path.length;i++){
+      for(var j = 0; j<2;j++){
+        if (j==0){
+          positionX = path[i][j];
+          
+        } else {
+          positionY=path[i][j];
+          
+        }
+      }
+      ctx.drawImage(cheminImg,positionX * 25,positionY * 25,tailleCases,tailleCases);
+      //grille[i][j] = "chemin";
+    }
+    
+    for(var b =0; b< 1300; b+=tailleCases)//faire des bons en largeur
+    {
+      coordX++;
+      var vert=0;
+      var bleu = 0;
+      var noir =0;
+      
+      for(var g = 0; g < tailleCases; g++)//on s'occupe pixel par pixel hauteur
+      {
+        ocean = 0;
+        plaine = 0;
+        montagne = 0;
+        
+        for(var h = 0; h < tailleCases; h++)//on s'occupe pixel par pixel largeur
+        {
+          var pix = h+(g*1300)+b+(a*1300);// h = Pour aller de 0 a 50, ensuite on va au pixels numéro 1300
+          var y = tabR[pix];
+          
+          if ( y< 255 &&  y >= 170) //ocean.
+          {
+            ocean++;
+          }
+          
+          if ( y < 170 &&  y > 85) //plaine.
+          {
+            plaine++;
+          }
+          
+          if ( y <= 85 &&  y >= 0) //montagne
+          {
+            montagne++;
+          }
+          
+        }
+        
+        if(montagne > plaine && montagne > ocean )
+        {
+          noir++;
+        }
+        
+        if(plaine > montagne && plaine > ocean)
+        {
+          vert++;
+        }
+        
+        if(ocean > plaine && ocean > montagne)
+        {
+          bleu++;
+        }
+        
+      }
+      
+      if(noir>vert && noir>bleu)
+      {
+        ctx.drawImage(montagneImg,b,a,tailleCases,tailleCases);
+        tabCoord[coordX][coordY] = "montagne";
+        
+        /*
+        if(coordX >= 2)
+        {
+        if(tabCoord[coordX - 2][coordY] == "montagne" && tabCoord[coordX][coordY] == "montagne")
+        {
+        ctx.drawImage(montagneImg,b-tailleCases,a,tailleCases,tailleCases);
+        tabCoord[coordX-1][coordY]=="montagne";
+      }
+      
+      
+    }
+    if(coordY >= 2)
+    {
+    if(tabCoord[coordX ][coordY - 2] == "montagne" && tabCoord[coordX ][coordY] == "montagne")
+    {
+    ctx.drawImage(montagneImg,b-tailleCases,a,tailleCases,tailleCases);
+    tabCoord[coordX][coordY-1]=="montagne";
+  }
+}
+*/
+
+}
+
+
+
+if(vert>noir && vert>bleu)
+{
+  
+  ctx.drawImage(plaineImg,b,a,tailleCases,tailleCases);
+  
+  tabCoord[coordX][coordY] = "plaine";
+  
+  /*
+  if(coordX >= 2)
+  {
+  if(tabCoord[coordX - 2][coordY] == "plaine" && tabCoord[coordX ][coordY] == "plaine")
+  {
+  ctx.drawImage(plaineImg,b-tailleCases,a,tailleCases,tailleCases);
+  
+  tabCoord[coordX-1][coordY]=="plaine";
+}
+}
+if(coordY >= 2)
+{
+if(tabCoord[coordX ][coordY - 2] == "plaine" && tabCoord[coordX ][coordY] == "plaine")
+{
+
+ctx.drawImage(plaineImg,b-tailleCases,a,tailleCases,tailleCases);
+
+tabCoord[coordX][coordY-1]=="plaine";
+}
+}
+*/
+}
+
+
+if(bleu>vert && bleu>noir)
+{
+  
+  ctx.drawImage(oceanImg,b,a,tailleCases,tailleCases);
+  
+  tabCoord[coordX][coordY] = "ocean";
+  /*
+  if(coordX >= 2)
+  {
+  if(tabCoord[coordX - 2][coordY] == "ocean" && tabCoord[coordX ][coordY] == "ocean")
+  {
+  
+  ctx.drawImage(oceanImg,b-tailleCases,a,tailleCases,tailleCases);
+  
+  tabCoord[coordX-1][coordY]="ocean";
+}
+}
+if(coordY >= 2)
+{
+if(tabCoord[coordX ][coordY - 2] == "ocean" && tabCoord[coordX ][coordY] == "ocean")
+{
+
+ctx.drawImage(oceanImg,b-tailleCases,a,tailleCases,tailleCases);
+
+tabCoord[coordX][coordY-1]=="ocean";
+}
+}
+*/
+}
+
+}
+}
+}
+
+function sleep(miliseconds) {
+  var currentTime = new Date().getTime();
+  
+  while (currentTime + miliseconds >= new Date().getTime()) {
+  }
 }
